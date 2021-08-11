f7d29804315d8cdce5bb643c402137ed6beda128
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-08-11 19:34:32 -0400

    commit tree f6f7ffb79bf30a4517179e343c26f03e31f42b85

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {changeDOM()}); 
+
+function changeDOM() {document.getElementById( "text" ).innerHTML = "This is really cool!"};
+
